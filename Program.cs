b302e7e13fb26a04fae0b3d8fc9b27ff44f7429d
Christopher Moran <chris.moran@emergent.com.au>
2018-06-06 16:28:30 +1000

    Initial Check-In

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace synth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            if(args.Length > 0)
+            {
+                foreach (String arg in args)
+                {
+                    try
+                    {
+                        synth.Speak(arg);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
